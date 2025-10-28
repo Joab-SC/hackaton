09da16dd9ceeacfc7cb3071d770879c3ef99469d
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2025-10-28 13:59:49 -0500

    feat: Se agregan funciones para escribir, borrar y actualizar un participante del csv

--- adapter/base_datos/base_datos_participante.exs
+++ adapter/base_datos/base_datos_participante.exs
@@ -50,4 +50,50 @@ defmodule Bd_Participante do
         []
     end
   end
+
+  def escribir_participante(nombre_archivo, %Participante{id: id, nombre: nombre, apellido: apellido, cedula: cedula,
+  correo: correo, telefono: telefono, usuario: usuario, contrasena: contrasena, id_equipo: id_equipo}) do
+    File.write(nombre_archivo, "\n#{id}, #{nombre}, #{apellido}, #{cedula}, #{correo}, #{telefono}, #{usuario}, #{contrasena}, #{id_equipo}", [:append, :utf8])
+  end
+
+
+  def borrar_participante(nombre_archivo, id_a_borrar) do
+    case File.read(nombre_archivo) do
+      {:ok, lista} ->
+        lineas = String.split(lista, "\n", trim: true)
+
+        [cabecera | datos] = lineas
+
+        nuevos_datos =
+          datos
+          |> Enum.reject(fn linea ->
+            case String.split(linea, ",") do
+              [id | _resto] -> id == id_a_borrar
+              _ -> false
+            end
+          end)
+
+        nuevo_contenido = Enum.join([cabecera | nuevos_datos], "\n")
+
+        case File.write(nombre_archivo, nuevo_contenido, [:utf8]) do
+          :ok ->
+            IO.puts("Participante con id #{id_a_borrar} eliminado correctamente.")
+            :ok
+
+          {:error, reason} ->
+            IO.puts("Error al escribir archivo: #{reason}")
+            {:error, reason}
+        end
+
+      {:error, reason} ->
+        IO.puts("Error al leer archivo: #{reason}")
+        {:error, reason}
+    end
+  end
+
+
+  def actualizar_participante(nombre_archivo, participante) do
+    borrar_participante(nombre_archivo, participante.id)
+    escribir_participante(nombre_archivo, participante)
+  end
 end
