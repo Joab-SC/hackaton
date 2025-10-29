5cb359286663ac76fe948509818f1d308cf9b390
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2025-10-29 10:56:18 -0500

    feat: Se realizan metodos para escribir, borrar y actaulizar equipo

--- domain/equipo.ex
+++ domain/equipo.ex
@@ -2,7 +2,7 @@ defmodule Equipo do
 
   defstruct id: "", nombre: "",  tema: ""
 
-  def crear_equipo(id, nombre, tema, participantes) do
+  def crear_equipo(id, nombre, tema) do
     %Equipo{id: id, nombre: nombre,  tema: tema}
   end
 
