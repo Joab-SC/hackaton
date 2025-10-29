5901a9606699a00a334bd6e38c40de94a2f12ae1
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2025-10-29 11:07:08 -0500

    feat: Se terminan los metodos para filtrar los mensajes según su tipo y tipo de receptor

--- adapter/base_datos/base_datos_mensaje.exs
+++ adapter/base_datos/base_datos_mensaje.exs
@@ -7,7 +7,7 @@ defmodule Bd_mensaje do
         case String.split(linea, ",") do
           ["id","Tipo_mensaje","Tipo_receptor","id_receptor","Contenido","id_equipo","Timestamp","id_proyecto"] -> nil
           [id,tipo_mensaje,tipo_receptor,id_receptor,contenido,id_equipo,timestamp,id_proyecto] ->
-            %Mensaje{id: id, tipo_mensaje: String.to_atom(tipo_mensaje), tipo_receptor: String.to_atom(tipo_receptor), id_receptor: id_receptor, contenido: contenido, timestamp: timestamp, id_proyecto: id_proyecto}
+            %Mensaje{id: id, tipo_mensaje: String.to_atom(tipo_mensaje), tipo_receptor: String.to_atom(tipo_receptor), id_receptor: id_receptor, contenido: contenido, id_equipo: id_equipo timestamp: timestamp, id_proyecto: id_proyecto}
           _ -> []
         end
       end)
@@ -23,20 +23,22 @@ defmodule Bd_mensaje do
     File.write(nombre_archivo, "\n#{id},#{Atom.to_string(tipo_mensaje)},#{Atom.to_string(tipo_receptor)},#{id_receptor},#{contenido},#{timestamp},#{id_proyecto}", [:append, :utf8])
   end
 
-  def filtrar_mensajes(nombre_archivo, tipo_buscar, id_buscar) do
-    cond do
-      tipo_buscar == :avance ->
-        Enum.filter(leer_mensajes(nombre_archivo), fn mensaje -> mensaje.id_proyecto == id_buscar end)
-      end
-    end
+  def filtrar_mensajes_proyecto(nombre_archivo, tipo_buscar, id_proyecto_buscar) do
+    Enum.filter(leer_mensajes(nombre_archivo), fn mensaje -> mensaje.tipo_mensaje == tipo_buscar and mensaje.id_proyecto == id_proyecto_buscar end)
+  end
+
+
+  def filtrar_mensajes(nombre_archivo, tipo_buscar, id_receptor_buscar) do
+      Enum.filter(leer_mensajes(nombre_archivo), fn mensaje -> mensaje.tipo_mensaje == tipo_buscar and mensaje.id_receptor == id_receptor_buscar end)
+  end
 
 
   def filtrar_mensajes(nombre_archivo, tipo_buscar) when tipo_buscar in [:avance, :chat, :consulta, :retroalimentacion, :anuncio] do
-    Enum.filter(leer_mensajes(nombre_archivo), fn mensaje -> mensaje.tipo== tipo_buscar end)
+    Enum.filter(leer_mensajes(nombre_archivo), fn mensaje -> mensaje.tipo_mensaje == tipo_buscar end)
   end
 
   def filtrar_mensajes(nombre_archivo, tipo_receptor) when tipo_receptor in [:equipo, :sala, :usuario, :todos] do
-    Enum.filter(leer_mensajes(nombre_archivo), fn mensaje -> mensaje.tipo_receptor== tipo_buscar end)
+    Enum.filter(leer_mensajes(nombre_archivo), fn mensaje -> mensaje.tipo_receptor== tipo_receptor end)
   end
 
 
