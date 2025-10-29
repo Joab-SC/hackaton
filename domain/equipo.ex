6d6a86cbb39fde4e0b014ba3929e844179624eb9
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2025-10-29 11:26:58 -0500

    feat: Se realiza las validaciones del equipo y se crea un servico general

--- domain/equipo.ex
+++ domain/equipo.ex
@@ -6,4 +6,18 @@ defmodule Equipo do
     %Equipo{id: id, nombre: nombre,  tema: tema}
   end
 
+  # ===========================
+  # VALIDACIONES PURAS
+  # ===========================
+
+  # Validar campos obligatorios
+  def validar_campos_obligatorios(nombre, tema) do
+    if Enum.any?([nombre, tema], &(&1 in ["", nil])) do
+      {:error, "El nombre y el tema del equipo son obligatorios."}
+    else
+      :ok
+    end
+  end
+
+
 end
